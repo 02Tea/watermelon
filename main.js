22a04c4c6bd309004c41e20e4839cf50db3480f3
Austin N <02teaa@gmail.com>
2023-01-08 23:09:05 +0800

    remove challenges

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
 import { watermelonRender } from "./watermelon.pfunc.js"
 // import { Watermelon } from "./watermelon.oop.js"
 
-/** 
- * Challenge rewrite watermelon.js as functional
- * try pure function approach
- * then have a separate version as OOP
- * 
- * source: https://javascript.plainenglish.io/javascript-functional-vs-oop-fb5fbf15a35d
- */
-
 // functional
 watermelonRender();
 
